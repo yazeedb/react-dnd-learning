28c72e1ce9279b05026cde534b871f9ee9d5e94e
Yazeed Bzadough <ybzadough@gmail.com>
2020-08-02 13:07:10 -0400

    persist drag/drop

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,58 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
 import { DragDropContext } from 'react-beautiful-dnd';
-import { initialData } from './initial-data';
+import { initialData, Task } from './initial-data';
 import { Column } from './Column';
 
 const App = () => {
   const [state, setState] = useState(initialData);
 
   return (
-    <DragDropContext onDragEnd={(result) => {}}>
+    <DragDropContext
+      onDragEnd={(result) => {
+        const { destination, source, draggableId } = result;
+
+        if (!destination) {
+          return;
+        }
+
+        if (
+          destination.droppableId === source.droppableId &&
+          destination.index === source.index
+        ) {
+          return;
+        }
+
+        const column = state.columns.get(source.droppableId);
+
+        if (!column) {
+          return;
+        }
+
+        const newTaskIds = [...column.taskIds];
+
+        newTaskIds.splice(source.index, 1);
+        newTaskIds.splice(destination.index, 0, draggableId);
+
+        setState({
+          ...state,
+          columns: state.columns.set(column.id, {
+            ...column,
+            taskIds: newTaskIds,
+          }),
+        });
+      }}
+    >
       {state.columnOrder.map((columnId) => {
-        // @ts-ignore
-        const column = state.columns[columnId] as Column;
-        // @ts-ignore
-        const tasks = column.taskIds.map((tId) => state.tasks[tId] as Task);
+        const column = state.columns.get(columnId);
+
+        if (!column) {
+          return <div />;
+        }
+
+        const tasks = column.taskIds.map((tId) =>
+          state.tasks.get(tId)
+        ) as Task[];
 
         return <Column column={column} tasks={tasks} key={columnId} />;
       })}
--- src/initial-data.ts
+++ src/initial-data.ts
@@ -1,17 +1,20 @@
 export const initialData = {
-  tasks: {
-    'task-1': { id: 'task-1', content: 'Take out the garbage' },
-    'task-2': { id: 'task-2', content: 'Watch my favorite show' },
-    'task-3': { id: 'task-3', content: 'Charge my phone' },
-    'task-4': { id: 'task-4', content: 'Cook dinner' },
-  },
-  columns: {
-    'column-1': {
-      id: 'column-1',
-      title: 'To do',
-      taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
-    },
-  },
+  tasks: new Map<string, Task>([
+    ['task-1', { id: 'task-1', content: 'Take out the garbage' }],
+    ['task-2', { id: 'task-2', content: 'Watch my favorite show' }],
+    ['task-3', { id: 'task-3', content: 'Charge my phone' }],
+    ['task-4', { id: 'task-4', content: 'Cook dinner' }],
+  ]),
+  columns: new Map<string, Column>([
+    [
+      'column-1',
+      {
+        id: 'column-1',
+        title: 'To do',
+        taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
+      },
+    ],
+  ]),
   // Facilitate reordering of the columns
   columnOrder: ['column-1'],
 };
